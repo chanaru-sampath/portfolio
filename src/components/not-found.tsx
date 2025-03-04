104074c7e1a04d293e2f07d5d0af4757c0359e9e
chanaru-sampath <chanarusampath@gmail.com>
2025-03-04 20:50:33 +0530

    Added not found route

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,16 +1,22 @@
-import { Link } from '@tanstack/react-router'
+import { Link, useLocation } from '@tanstack/react-router'
 
 const NotFound = () => {
+  const location = useLocation()
+
   return (
     <div className="flex items-center justify-center h-[500px]">
       <div className="text-center">
         <h1 className="text-5xl font-bold mb-4">
-          Ops<span className="text-accent">!</span>
+          Ops! <span className="text-accent">404</span>
         </h1>
         <p className="text-lg">
-          There is no route like this. Please check the url or{' '}
-          <Link className="underline text-accent" to="/">
-            Go to home page
+          There is no page for{''}
+          <span className="text-accent italic">{`'${location.pathname}'`}</span>
+        </p>
+        <p className="text-lg">
+          Please check the url or Go to{' '}
+          <Link className="underline text-accent capitalize" to="/">
+            home page.
           </Link>
         </p>
       </div>
