2ff34905ab626dd108d2c4aefa8ed859240b3f25
chanaru-sampath <chanarusampath@gmail.com>
2025-03-13 08:08:04 +0530

    Resume page

--- src/pages/resume-page.tsx
+++ src/pages/resume-page.tsx
@@ -1,7 +1,102 @@
-import ComingSoon from '@/components/coming-soon'
+import { FaReact, FaNodeJs, FaHtml5 } from 'react-icons/fa'
+import { SiRedux, SiTypescript } from 'react-icons/si'
+import { IoLogoJavascript, IoLogoFirebase } from 'react-icons/io5'
+import { RiNextjsFill } from 'react-icons/ri'
+import { IoLogoCss3 } from 'react-icons/io'
+import { DiGoogleCloudPlatform } from 'react-icons/di'
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+const personalInfo = {
+  title: 'About Me',
+  description:
+    'Senior Full-Stack JavaScript Engineer skilled in Next.js, Node.js, and React, focused on performance, clean code, and seamless user experiences.',
+  info: [
+    { name: 'name', value: 'Chanaru Sampath' },
+    { name: 'title', value: 'Senior Software Engineer' },
+    { name: 'experience', value: '7+ Years' },
+    { name: 'email', value: 'chanarusampath@gmail.com' },
+    { name: 'phone', value: '+94717155557' },
+    { name: 'address', value: 'Minuwangoda, Sri Lanka' },
+  ],
+}
+
+const skills = {
+  title: 'Skills',
+  description:
+    'Skilled in full-stack development with JavaScript, TypeScript, Next.js, Node.js, React, and database management. Experienced in performance optimization, API development, and scalable architectures',
+  items: [
+    { name: 'JavaScript', icon: IoLogoJavascript },
+    { name: 'TypeScript', icon: SiTypescript },
+    { name: 'Html', icon: FaHtml5 },
+    { name: 'Css', icon: IoLogoCss3 },
+    { name: 'ReactJs', icon: FaReact },
+    { name: 'NextJs', icon: RiNextjsFill },
+    { name: 'NodeJs', icon: FaNodeJs },
+    { name: 'Redux', icon: SiRedux },
+    { name: 'Firebase', icon: IoLogoFirebase },
+    { name: 'Google Cloud', icon: DiGoogleCloudPlatform },
+  ],
+}
 
 const ResumePage = () => {
-  return <ComingSoon />
+  return (
+    <div className="h-[80vh] w-full flex items-start justify-center py-2 lg:py-0">
+      <Tabs
+        defaultValue="skills"
+        className="flex flex-col lg:flex-row gap-6 lg:gap-[60px] w-full"
+      >
+        <TabsList className="flex flex-col w-full gap-4 max-w-[380px] mx-auto lg:mx-0">
+          <TabsTrigger value="experience">Experience</TabsTrigger>
+          <TabsTrigger value="education">Education</TabsTrigger>
+          <TabsTrigger value="skills">Skills</TabsTrigger>
+          <TabsTrigger value="aboutme">About Me</TabsTrigger>
+        </TabsList>
+        <div className="">
+          <TabsContent value="experience">experience</TabsContent>
+          <TabsContent value="education">education</TabsContent>
+          <TabsContent value="skills">
+            <div className="flex flex-col items-center lg:items-start">
+              <h1 className="text-2xl mb-2 lg:mb-4">{skills.title}</h1>
+              <p className="text-white/80 text-center lg:text-start leading-6">
+                {skills.description}
+              </p>
+              <div className="grid gap-4 text-5xl grid-cols-4  lg:grid-cols-5 xl:grid-cols-6 mt-4 lg:mt-6">
+                {skills.items.map((skill) => (
+                  <div className="w-[80px] h-[80px] bg-background-secondary rounded-lg">
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger className="flex items-center justify-center w-full h-full">
+                          <skill.icon />
+                        </TooltipTrigger>
+                        <TooltipContent side="bottom">
+                          <p>{skill.name}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="aboutme">
+            <div className="flex flex-col items-center lg:items-start">
+              <h1 className="text-2xl mb-2 lg:mb-4">{personalInfo.title}</h1>
+              <p className="text-white/80 text-center lg:text-start leading-6">
+                {personalInfo.description}
+              </p>
+            </div>
+          </TabsContent>
+        </div>
+      </Tabs>
+    </div>
+  )
 }
 
 export default ResumePage
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
@@ -9,6 +10,27 @@ export default defineConfig({
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     tailwindcss(),
     react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
+      injectManifest: {
+        swDest: 'dist/sw.js',
+      },
+      manifest: {
+        name: 'Chanaru Sampath',
+        short_name: 'CS.',
+        theme_color: 'oklch(87.91% 0.2134 155.95)',
+        background_color: 'oklch(20.99% 0.0341 263.44)',
+        start_url: '/',
+        display: 'standalone',
+        orientation: 'portrait',
+      },
+    }),
   ],
   server: {
     port: 3000,
