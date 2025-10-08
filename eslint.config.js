4d2cd2cc47692df50831f243b0663bd71bd8faa9
chanaru-sampath <chanarusampath@gmail.com>
2025-10-08 08:03:20 +0530

    git ignore some folders

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,11 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
+    optimizeDeps: {
+      exclude: ['sw.js'], // or the package name if sw.js is from a package
+    },
+  },
+  {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
