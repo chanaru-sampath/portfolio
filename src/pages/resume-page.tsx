f663019c8439df26f9211bee07485472adf53716
chanaru-sampath <chanarusampath@gmail.com>
2025-12-24 08:14:58 +0530

    add page transision

--- src/pages/resume-page.tsx
+++ src/pages/resume-page.tsx
@@ -5,6 +5,7 @@ import { IoLogoFirebase, IoLogoJavascript } from 'react-icons/io5'
 import { RiNextjsFill } from 'react-icons/ri'
 import { SiNestjs, SiRedux, SiTypescript } from 'react-icons/si'
 
+import PageTransition from '@/components/page-transition'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import {
   Tooltip,
@@ -59,69 +60,71 @@ const skills = {
 
 const ResumePage = () => {
   return (
-    <div className="h-[80vh] w-full flex items-start justify-center py-2 lg:py-0">
-      <Tabs
-        defaultValue="skills"
-        className="flex flex-col lg:flex-row gap-6 lg:gap-[60px] w-full"
-      >
-        <TabsList className="flex flex-col w-full gap-4 max-w-[380px] mx-auto lg:mx-0">
-          <TabsTrigger value="experience">Experience</TabsTrigger>
-          <TabsTrigger value="education">Education</TabsTrigger>
-          <TabsTrigger value="skills">Skills</TabsTrigger>
-          <TabsTrigger value="aboutme">About Me</TabsTrigger>
-        </TabsList>
-        <div className="">
-          <TabsContent value="experience">
-            <div className="flex flex-col items-center lg:items-start">
-              <h1 className="text-2xl mb-2 lg:mb-4">{experiences.title}</h1>
-              <p className="text-white/80 text-center lg:text-start leading-6">
-                {experiences.description}
-              </p>
-            </div>
-          </TabsContent>
-          <TabsContent value="education">
-            <div className="flex flex-col items-center lg:items-start">
-              <h1 className="text-2xl mb-2 lg:mb-4">{education.title}</h1>
-              <p className="text-white/80 text-center lg:text-start leading-6">
-                {education.description}
-              </p>
-            </div>
-          </TabsContent>
-          <TabsContent value="skills">
-            <div className="flex flex-col items-center lg:items-start">
-              <h1 className="text-2xl mb-2 lg:mb-4">{skills.title}</h1>
-              <p className="text-white/80 text-center lg:text-start leading-6">
-                {skills.description}
-              </p>
-              <div className="grid gap-4 text-5xl grid-cols-4  lg:grid-cols-5 xl:grid-cols-6 mt-4 lg:mt-6">
-                {skills.items.map((skill) => (
-                  <div className="w-[80px] h-[80px] bg-background-secondary rounded-lg">
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger className="flex items-center justify-center w-full h-full">
-                          <skill.icon />
-                        </TooltipTrigger>
-                        <TooltipContent side="bottom">
-                          <p>{skill.name}</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </div>
-                ))}
+    <PageTransition>
+      <div className="h-[80vh] w-full flex items-start justify-center py-2 lg:py-0">
+        <Tabs
+          defaultValue="skills"
+          className="flex flex-col lg:flex-row gap-6 lg:gap-[60px] w-full"
+        >
+          <TabsList className="flex flex-col w-full gap-4 max-w-[380px] mx-auto lg:mx-0">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="aboutme">About Me</TabsTrigger>
+          </TabsList>
+          <div className="">
+            <TabsContent value="experience">
+              <div className="flex flex-col items-center lg:items-start">
+                <h1 className="text-2xl mb-2 lg:mb-4">{experiences.title}</h1>
+                <p className="text-white/80 text-center lg:text-start leading-6">
+                  {experiences.description}
+                </p>
               </div>
-            </div>
-          </TabsContent>
-          <TabsContent value="aboutme">
-            <div className="flex flex-col items-center lg:items-start">
-              <h1 className="text-2xl mb-2 lg:mb-4">{personalInfo.title}</h1>
-              <p className="text-white/80 text-center lg:text-start leading-6">
-                {personalInfo.description}
-              </p>
-            </div>
-          </TabsContent>
-        </div>
-      </Tabs>
-    </div>
+            </TabsContent>
+            <TabsContent value="education">
+              <div className="flex flex-col items-center lg:items-start">
+                <h1 className="text-2xl mb-2 lg:mb-4">{education.title}</h1>
+                <p className="text-white/80 text-center lg:text-start leading-6">
+                  {education.description}
+                </p>
+              </div>
+            </TabsContent>
+            <TabsContent value="skills">
+              <div className="flex flex-col items-center lg:items-start">
+                <h1 className="text-2xl mb-2 lg:mb-4">{skills.title}</h1>
+                <p className="text-white/80 text-center lg:text-start leading-6">
+                  {skills.description}
+                </p>
+                <div className="grid gap-4 text-5xl grid-cols-4  lg:grid-cols-5 xl:grid-cols-6 mt-4 lg:mt-6">
+                  {skills.items.map((skill) => (
+                    <div className="w-[80px] h-[80px] bg-background-secondary rounded-lg">
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger className="flex items-center justify-center w-full h-full">
+                            <skill.icon />
+                          </TooltipTrigger>
+                          <TooltipContent side="bottom">
+                            <p>{skill.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </TabsContent>
+            <TabsContent value="aboutme">
+              <div className="flex flex-col items-center lg:items-start">
+                <h1 className="text-2xl mb-2 lg:mb-4">{personalInfo.title}</h1>
+                <p className="text-white/80 text-center lg:text-start leading-6">
+                  {personalInfo.description}
+                </p>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </PageTransition>
   )
 }
 
